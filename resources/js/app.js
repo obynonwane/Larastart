b5c8f6c306a8411c4961732955470128f2f75a03
OBINNA <biostechnologyng@gmail.com>
2019-08-20 04:23:09 -0700

    WOrking on Template

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,19 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue') },
+    { path: '/profile', component: require('./components/Profile.vue') }
+  ]
+
+  const router = new VueRouter({
+    routes // short for `routes: routes`
+  })
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -29,4 +42,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
