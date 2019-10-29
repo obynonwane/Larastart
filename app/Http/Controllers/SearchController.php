10fba4ce5ad75cba152e423e31d4908bfbb46528
OBINNA <biostechnologyng@gmail.com>
2019-10-29 00:55:17 -0700

    Updates

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class SearchController extends Controller
 {
     
     public function index(){
-        
+
+        $data = request()->validate([
+            'searchTerm' => 'required'
+        ]);
+
+        return User::search($data['searchTerm'])->get();
+
     }
 }
